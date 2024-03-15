f2a180a62ce254d4bf749f5ab0682a8d4514788d
patrick <patrick@ms.touk.io>
2024-03-15 15:43:38 -0300

    home button

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,9 +1,9 @@
 import React, { useState } from 'react';
 import styles from './Navbar.module.css';
-import { useNavigate } from 'react-router-dom'; 
+import { useNavigate } from 'react-router-dom';
 
 interface NavbarProps {
-  userPhoto?: string; 
+  userPhoto?: string;
   userName: string;
 }
 
@@ -12,42 +12,48 @@ const Navbar: React.FC<NavbarProps> = ({ userPhoto, userName }) => {
   const searchIcon = 'searchicon.png';
 
   const navigate = useNavigate();
-  
-  const [searchText, setSearchText] = useState(''); 
+
+  const [searchText, setSearchText] = useState('');
 
   const handleSearchChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    setSearchText(e.target.value); 
+    setSearchText(e.target.value);
   };
 
   const handleSearchSubmit = async () => {
     try {
-        navigate('/search', { state: { text: searchText} });
+      navigate('/search', { state: { text: searchText } });
     } catch (error) {
       console.error('Erro na busca:', error);
       alert('Error on finding games. Try again.');
     }
   };
 
+  const handleHomeClick = () => {
+    navigate('/dashboard');
+  };
+
   return (
     <nav className={styles.navbar}>
       <div className={styles.userInfo}>
-      <img src={userPhoto} alt="User" className={styles.userPhoto}/>
+        <img src={userPhoto} alt="User" className={styles.userPhoto} />
         <span className={styles.userName}>{userName}</span>
       </div>
       <ul className={styles.navbarItems}>
         {menuItems.map((item, index) => (
           <li key={index}>
-            <a href={`#${item}`}>{item}</a>
+            <a href={`#${item}`} onClick={item === 'Home' ? handleHomeClick : undefined}>
+              {item}
+            </a>
           </li>
         ))}
       </ul>
       <div className={styles.searchBox}>
-        <input 
-          type="text" 
-          placeholder="Search..." 
-          className={styles.searchInput} 
-          value={searchText} 
-          onChange={handleSearchChange} 
+        <input
+          type="text"
+          placeholder="Search..."
+          className={styles.searchInput}
+          value={searchText}
+          onChange={handleSearchChange}
           onKeyDown={(e) => e.key === 'Enter' && handleSearchSubmit()}
         />
         <img src={searchIcon} alt="Search" className={styles.searchIcon} />
