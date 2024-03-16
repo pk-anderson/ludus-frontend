9d0301fc01d670ea6097990d5c6f9ead74836e15
patrick <patrick@ms.touk.io>
2024-03-16 18:12:23 -0300

    fix mycollection

--- src/screens/Collection/Collection.tsx
+++ src/screens/Collection/Collection.tsx
@@ -1,18 +1,36 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
 import Button from '../../components/Button/Button';
 import styles from './Collection.module.css';
-import { useLocation } from 'react-router-dom';
+import { listLibrary } from '../../api/userLibrary';
 
 const Collection: React.FC = () => {
     const [currentPage, setCurrentPage] = useState(1);
+    const [collection, setCollection] = useState<any>(null); 
     const gamesPerPage = 9;
-    const location = useLocation();
-    const games = location.state?.games || [];
+    const storedUser = JSON.parse(localStorage.getItem('user') || '{}');
+    const currentUserId = storedUser.id || 0;
+    const [currentGames, setCurrentGames] = useState<any[]>([]);
+
+    useEffect(() => {
+        const fetchCollection = async () => {
+            try {
+                const response = await listLibrary(currentUserId);
+                setCollection(response.result);
+            } catch (error) {
+                console.error('Error fetching collection:', error);
+            }
+        };
     
-    const indexOfLastGame = currentPage * gamesPerPage;
-    const indexOfFirstGame = indexOfLastGame - gamesPerPage;
-    const currentGames = games.slice(indexOfFirstGame, indexOfLastGame);
+        fetchCollection();
+    }, [currentUserId]);
+    
+    useEffect(() => {
+        const indexOfLastGame = currentPage * gamesPerPage;
+        const indexOfFirstGame = indexOfLastGame - gamesPerPage;
+        const gamesToShow = collection ? collection.slice(indexOfFirstGame, indexOfLastGame) : [];
+        setCurrentGames(gamesToShow);
+    }, [collection, currentPage, gamesPerPage]);
 
     const handleNextPage = () => {
         setCurrentPage(prevPage => prevPage + 1);
@@ -53,7 +71,7 @@ const Collection: React.FC = () => {
                 <button className={styles.previousButton} onClick={handlePrevPage} disabled={currentPage === 1}>
                     Previous
                 </button>
-                <button className={styles.nextButton} onClick={handleNextPage} disabled={currentPage === Math.ceil(games.length / gamesPerPage)}>
+                <button className={styles.nextButton} onClick={handleNextPage} disabled={collection && currentPage === Math.ceil(collection.length / gamesPerPage)}>
                     Next
                 </button>
             </div>
